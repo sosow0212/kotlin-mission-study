4b0358eac8b98ce7e0c84ab184e3f494ccaf89d5
sosow0212 <sosow0212@naver.com>
2024-10-10 21:25:21 +0900

    feat: 로또 게임 완성

--- console-lotto-mission/src/main/kotlin/Main.kt
+++ console-lotto-mission/src/main/kotlin/Main.kt
@@ -1,5 +1,16 @@
 package com
 
+import com.controller.LottoController
+import com.model.service.LottoNumberGenerator
+import com.view.InputView
+import com.view.OutputView
+
 fun main() {
-    println("Hello World!")
+    val lottoController = LottoController(
+        InputView(),
+        OutputView(),
+        LottoNumberGenerator()
+    )
+
+    lottoController.run()
 }
